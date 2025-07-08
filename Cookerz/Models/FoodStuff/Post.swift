b913e362bd679aa8cd597764a0cec916592db147
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-08 12:54:51 +0200

    Update Post.swift comments

--- Cookerz/Models/FoodStuff/Post.swift
+++ Cookerz/Models/FoodStuff/Post.swift
@@ -10,17 +10,20 @@ import Foundation
 struct Post : Identifiable{
     var id  : UUID = UUID()
     var creatorId : UUID
+    //There is null (?) because we aren't obliged to give one
     var title : String?
     var image : String
     var date : Date
     var level : Level
     var likes : Int
+    //Same
     var durationHours : Int?
     var durationMinutes : Int?
     
     
-    // défi
+    // This is used if the post is to confirm that a challenge has been completed.
     var isChallenge : Bool
+    //There is null (?) because if it's not, we don't have a challenge ID
     var challengeId : UUID?
 }
 
