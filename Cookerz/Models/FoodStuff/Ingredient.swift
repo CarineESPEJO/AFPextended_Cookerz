ac855372888cdb2a10baefd543c376d7df784a89
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-08 11:57:37 +0200

    Update Ingredient.swift

--- Cookerz/Models/FoodStuff/Ingredient.swift
+++ Cookerz/Models/FoodStuff/Ingredient.swift
@@ -10,7 +10,9 @@ import Foundation
 struct Ingredient : Identifiable, Hashable{
     var id  : UUID = UUID()
     var name : String
+    // Enum
     var type : IngredientType
+    // null (?) exist for the wheel to choose ingredient when we add ingredient when we create recipe or workshop before we can 
     var quantity : Double?
     var unit : IngredientUnit?
 }
