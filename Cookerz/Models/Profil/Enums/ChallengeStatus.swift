e6c6a8093973c3205c4d23cbb2d13c788ebc36b6
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-10 08:24:01 +0200

    Update ChallengeStatus.swift comment

--- Cookerz/Models/Profil/Enums/ChallengeStatus.swift
+++ Cookerz/Models/Profil/Enums/ChallengeStatus.swift
@@ -5,6 +5,7 @@
 //  Created by Carole TREM on 14/03/2025.
 //
 
+//enum of the possible different states of the the challenges
 enum ChallengeStatus : Hashable, CaseIterable {
     case isNotStarted
     case onGoing
