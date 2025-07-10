57cfcd5a9b2c285864765133b8d42a61a7d6c6f1
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-10 08:23:08 +0200

    Update Badge.swift comments

--- Cookerz/Models/Profil/Enums/Badge.swift
+++ Cookerz/Models/Profil/Enums/Badge.swift
@@ -2,9 +2,11 @@
 //  Badges.swift
 //  Cookerz
 //
-//  Created by apprenant98 on 07/03/2025.
+//  Created by Carine ESPEJO on 07/03/2025.
 //
 
+// we wanted to give to the users a rank on their profil under their image
+// it's based on the number of challenge done
 enum Badge {
     case noBadge
     case newbie
