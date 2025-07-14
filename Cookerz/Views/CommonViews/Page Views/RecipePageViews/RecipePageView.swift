cd71ef7fdbe9bfd77774a1de36f35098acfc1100
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-14 10:32:29 +0200

    Update RecipePageView.swift comments

--- Cookerz/Views/CommonViews/Page Views/RecipePageViews/RecipePageView.swift
+++ Cookerz/Views/CommonViews/Page Views/RecipePageViews/RecipePageView.swift
@@ -2,14 +2,15 @@
 //  RecipePageView.swift
 //  Cookerz
 //
-//  Created by apprenant98 on 10/03/2025.
+//  Created by Carine ESPEJO on 10/03/2025.
 //
 
 import SwiftUI
 
 struct RecipePageView: View {
-
+    // link the recipe to show from parent view (the button clicked to go on the page)
     @State var recipe: Recipe
+    // go retrieve its creator infos by theit UUID
     var recipeCreatorId: UUID { recipe.creatorId }
     var recipeCreator: User? {
         for user in users {
@@ -19,19 +20,28 @@ struct RecipePageView: View {
         }
         return nil
     }
-    
+
+    // var to change number of portions (
     @State var selectedPart: RecipeEnum = .ingredients
+    // var to change number of portions 
+    // (we didn't have time to change, but if I can sometime, 
+    // the number PortionChosen may be better to be started on the number portion chosen by the creator of the recipe)
     @State var numberPortionChosen: Int = 1
     
     var body: some View {
         ZStack {
+            //BG color
             Color.white.ignoresSafeArea()
-            
+
+            // the page is scrollable
             ScrollView {
+                
                 ZStack(alignment: .top) {
+                    // recipe image under the rest of the page to be able to have a rounder corner on the background of the rest
                     BackgroungImage(imageName: recipe.image)
                     
                     VStack(alignment: .leading, spacing: 10) {
+                        // function to let a space at the top before the rest to see the recipe image behind
                         RecipePageTransparentFrontBGImage(recipe: recipe)
                         
                         
