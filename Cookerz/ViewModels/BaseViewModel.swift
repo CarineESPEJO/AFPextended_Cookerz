ad1dbe36f8227528eb3996613dd56521c8f04af3
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-14 10:20:29 +0200

    Update BaseViewModel.swift comments

--- Cookerz/ViewModels/BaseViewModel.swift
+++ Cookerz/ViewModels/BaseViewModel.swift
@@ -3,6 +3,7 @@
 //  Cookerz
 //
 //  Created by apprenant87 on 13/03/2025.
+//  Code added by Carine ESPEJO from line to 170 to 280
 //
 
 import Foundation
@@ -165,33 +166,49 @@ class BaseViewModel: ObservableObject {
     var currentPoints : Int {
         totalPointsGained - totalPointsUsed
     }
+
+    // Here my the modelview for my part we had to put in there
+    // Someone else was chosen to take care of the model view part. However, instead of creating several model views, they only made one.
+    // Linking between certain parts of the back-end code was difficult due to that, and we had to adapt as much as we could at the end to make it work. 
+    // Thus, the back-end of recipe creation is located in the model view, but the one of the live creation is on a separate page.
     
     //MARK: -  add recipes
-    
+
+
+    // var to change between the 2 parts of the creation page
     @Published var selectedPart: RecipeEnum = .ingredients
+
+    // variables needed to create a recipe
     @Published var recipeTitle: String = ""
+    // it can be null because there is no value before we enter a name for the recipe.
     @Published var recipeImage: String? = nil
-    
+    // a recipe can be in several categories (ex: asiatic and vegan)
     @Published var selectedRecipeTypes: [RecipeType] = []
     
     @Published var recipeTimeHours: Int = 0
     @Published var recipeTimeMinutes: Int = 0
     @Published var recipedifficulty: Level = .defaultdifficulty
     @Published var recipePortionNb: Int = 1
-    
+
+    // var to contain the recipe ingredients list
     @Published var listIngredients: [Ingredient] = []
+    // var to choose a new ingredient before adding it to the recipe ingredient list
     @Published var newIngredient: Ingredient = ingredientsBase[0]
+    // Same bur it's quantity and unit
     @Published var newIngredientQuantity: String = ""
     @Published var newIngredientUnit: IngredientUnit = .gramme
-    
+
+    // same the recipe ustensils list
     @Published var listUstensils: [Ustensil] = []
     @Published var newUstensilType: UstensilType = .cookerMachine
     @Published var newUstensilName: String = ""
-    
+
+    // same the recipe steps list
     @Published var listSteps: [String] = []
     @Published var newStep: String = ""
     
-
+    // var to let the user to confirm the creation of the page only when it enter each fields : 
+    //add an image, a name, a type, a duration, a difficulty and at least one ingredient and one step
     var isFormComplete: Bool {
         return recipeImage != nil
         && !recipeTitle.isEmpty
@@ -201,32 +218,39 @@ class BaseViewModel: ObservableObject {
         && !listIngredients.isEmpty
         && !listSteps.isEmpty
     }
-    
+
+    // function to add an ingredient to the list of ingredients
     func addIngredient() {
+        // Verify if the quantity has been added and convert it into a double to be modified when changing the number of portions on the recipe page.
         if let quantity = Double(newIngredientQuantity), !newIngredientQuantity.isEmpty {
+            // add ingredient to the the list of ingredients (name,type,quantity,unit)
             listIngredients.append(Ingredient(name: newIngredient.name, type: newIngredient.type, quantity: quantity, unit: newIngredientUnit))
+            //reset the quantity to none to add new ingredient
             newIngredientQuantity = ""
         }
     }
-    
+
+    //function to remove an ingredient from the ingredients list
     func removeIngredient(at index: Int) {
         listIngredients.remove(at: index)
     }
-    
+
+    // func to add a step to the steps list
     func addStep() {
         if !newStep.isEmpty {
             listSteps.append(newStep)
             newStep = ""
         }
     }
-    
+
+    //function to remove a step from the steps list
     func removeStep(at index: Int) {
         listSteps.remove(at: index)
     }
     
-
+// func to create the recipe 
     func postRecipe(dismiss: @escaping () -> Void) {
- 
+        // create a var which take  all the fields info
         let newRecipe = Recipe(
             creatorId: user.id,
             title: recipeTitle,
@@ -243,16 +267,23 @@ class BaseViewModel: ObservableObject {
             isLiked: false
         )
         
-
+        // add to the list of recipes recipesVM the var above
         recipesVM.insert(newRecipe, at: 0)
+        // just to verify that the recipe has been added correctly
         print(recipesVM)
         
-
+        // close the recipe creation page
         dismiss()
     }
     
     @Published var isGalleryPermissionRequested: Bool = false
 }
+
+
+
+
+
+
 
 //    func toggleFavoriteRecipe(recipe: Recipe) {
 //        if !user.listRecipes.contains(where: { $0.id == recipe.id }) {
