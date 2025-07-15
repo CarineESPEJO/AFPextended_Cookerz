5cf1fbd014ab37b8a0d0b5e4a6fcb594039da4a7
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-15 11:36:32 +0200

    Update AtelierEnum.swift comments

--- Cookerz/Models/ViewsStuff/Enums/AtelierEnum.swift
+++ Cookerz/Models/ViewsStuff/Enums/AtelierEnum.swift
@@ -2,9 +2,10 @@
 //  AtelierEnum.swift
 //  Cookerz
 //
-//  Created by apprenant?? on 13/03/2025.
+//  Created by Carine ESPEJO on 13/03/2025.
 //
 
+// enum to switch between the 2 parts (description and ingredients) of the cooking classes Pages and cooking classes creation Page
 enum AtelierEnum {
     case description
     case ingredients
