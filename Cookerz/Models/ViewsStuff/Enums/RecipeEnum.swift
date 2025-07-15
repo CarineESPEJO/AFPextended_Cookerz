94883d6b9f2e54ea20a19d12bdd429fbd5719b8d
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-15 11:35:07 +0200

    Update RecipeEnum.swift comments

--- Cookerz/Models/ViewsStuff/Enums/RecipeEnum.swift
+++ Cookerz/Models/ViewsStuff/Enums/RecipeEnum.swift
@@ -2,9 +2,10 @@
 //  RecipeEnum.swift
 //  Cookerz
 //
-//  Created by apprenant?? on 07/03/2025.
+//  Created by Carine ESPEJO on 07/03/2025.
 //
 
+// enum to switch between the 2 parts (ingredients and steps) of the recipes Pages and recipes creation Pages
 enum RecipeEnum {
     case ingredients
     case instructions
