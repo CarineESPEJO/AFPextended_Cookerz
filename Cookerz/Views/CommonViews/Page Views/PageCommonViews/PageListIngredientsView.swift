3e0cf5b4cffd36be04e3daf407f88289f937812b
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-15 09:00:25 +0200

    Update PageListIngredientsView.swift comments

--- Cookerz/Views/CommonViews/Page Views/PageCommonViews/PageListIngredientsView.swift
+++ Cookerz/Views/CommonViews/Page Views/PageCommonViews/PageListIngredientsView.swift
@@ -2,16 +2,20 @@
 //  PageListIngredientsView.swift
 //  Cookerz
 //
-//  Created by apprenant98 on 11/03/2025.
+//  Created by Carine ESPEJO on 11/03/2025.
 //
 
 import SwiftUI
 
 struct PageListIngredientsView: View {
+    // list of ingredients
     var listIngredient: [Ingredient]
+    //number of portion put by the creator to do conversions
     var portion : Int
+    // Binding to change the number of portions
     @Binding var numberPortionChosen : Int
-    
+
+    // function to transform the format into x.xx or x if x.00
     func formatNumber(_ number: Double) -> String {
       
         let formattedNumber = String(format: "%.2f", number)
@@ -23,10 +27,17 @@ struct PageListIngredientsView: View {
             return formattedNumber
         }
     }
-    
+
+    //function to modify the quantity of the ingredients based on the number of portions
     func calculateAdjustedQuantity(for ingredient: Ingredient) -> String {
+        // as ingredient as quantity and unit whith ? (can be null) for the creation
+        // its for bypass it by verifying that they are non-null 
+        //(can't be null because recipes and cooking classes can't be confirmed if one is null)
         if let quantity = ingredient.quantity, let unit = ingredient.unit {
+            // create variable adapted by the portion chosen by the user 
+            // its quantity for one portion multiplied by the number chosen
             let adjustedQuantity = quantity * Double(numberPortionChosen) / Double(portion)
+            // Format this new quantity as x.xx.
             let formattedQuantity = formatNumber(adjustedQuantity)
             return "\(formattedQuantity) \(unit.initial)"
         }
@@ -36,15 +47,17 @@ struct PageListIngredientsView: View {
     var body: some View {
         ForEach(listIngredient) { ingredient in
             HStack {
+                // icon of the type of the ingredient
                 Image(ingredient.type.Icon)
                     .resizable()
                     .frame(width: 24, height: 24)
-                
+
+                //name formatted in capital and then in lowercase
                 Text(ingredient.name.capitalized)
                     .font(.PoppinsCallout())
                 
                 Spacer()
-                
+                // quantity based on the number of portions
                 Text(calculateAdjustedQuantity(for: ingredient))
                     .font(.PoppinsSubhead())
                     .foregroundStyle(.black.opacity(0.7))
