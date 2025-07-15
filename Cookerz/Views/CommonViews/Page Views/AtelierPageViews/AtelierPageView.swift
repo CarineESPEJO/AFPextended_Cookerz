d6f25b1377c08c49c44d86e7dbd5743b9a91b8bf
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-15 07:42:07 +0200

    Update AtelierPageView.swift comments

--- Cookerz/Views/CommonViews/Page Views/AtelierPageViews/AtelierPageView.swift
+++ Cookerz/Views/CommonViews/Page Views/AtelierPageViews/AtelierPageView.swift
@@ -2,12 +2,14 @@
 //  AtelierPageView.swift
 //  Cookerz
 //
-//  Created by apprenant98 on 10/03/2025.
+//  Created by Carine ESPEJO on 10/03/2025.
 //
 
 import SwiftUI
+// link the cooking class to show from parent view (the button clicked to go on the page)
 struct AtelierPageView: View {
     var atelier: Atelier
+    // go retrieve its creator infos by theit UUID
     var atelierCreatorId: UUID { atelier.creatorId }
     var atelierCreator: User? {
         for user in users {
@@ -17,22 +19,32 @@ struct AtelierPageView: View {
         }
         return nil
     }
-    
+
+    // var to change page between the presentation part and the ingredients part
     @State var selectedPart: AtelierEnum = .description
+    // var to change number of portions 
+    // (the number PortionChosen is initialized with 1 to make it work 
+    // but when the page appears it takes the number Portion chosen by the creator)
     @State var numberPortionChosen: Int = 1
     
     var body: some View {
         ZStack {
+            //BG color
             Color.white.ignoresSafeArea()
             
+            // the page is scrollable
             ScrollView {
                 ZStack(alignment: .top) {
+                    // cooking class image under the rest of the page to be able to have a rounder corner on the background of the rest
                     BackgroungImage(imageName: atelier.image)
                     
                     VStack(alignment: .leading, spacing: 10) {
+                        // to let a space at the top before the rest to see the coocking class image behind
+                        // with return button
                         AtelierPageTransparentFrontBGImage()
                         
                         VStack {
+                            // infos of the creator
                             PageHeaderView(elementTitle: atelier.title, recipeCreator: atelierCreator)
                             
                             HStack(spacing: 8) {
@@ -42,11 +54,13 @@ struct AtelierPageView: View {
                             }
                             
                             AtelierDoublePickerView(selectedPart: $selectedPart)
-                        
+
+                            // if description part, show the description of the cooking class
                             if selectedPart == .description {
                                 Text("\(atelier.description)")
                                     .font(.PoppinsSubhead())
                             }
+                            // else it shows the ingredients and ustensils
                                 else {
                                     PageStuffView( numberPortionChosen: $numberPortionChosen, numberPortionBase: atelier.portion, listIngredients : atelier.ingredients,
                                                    listUstensils : atelier.ustensils)
@@ -63,8 +77,10 @@ struct AtelierPageView: View {
                 .padding(.top, 8)
             }
         }
+        // return button is hidden because there is another one customed
         .navigationBarBackButtonHidden(true)
         .ignoresSafeArea()
+        // when page appear, numberPortionChosen takes portion chosen by creator as base
         .onAppear {
             numberPortionChosen = atelier.portion
         }
