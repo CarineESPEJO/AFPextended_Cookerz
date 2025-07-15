d43e5d8b2e7a9568fade5ac56aa42abb0e3e21ba
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-15 07:52:47 +0200

    Update AtelierPageDateView.swift comments

--- Cookerz/Views/CommonViews/Page Views/AtelierPageViews/AtelierPageDateView.swift
+++ Cookerz/Views/CommonViews/Page Views/AtelierPageViews/AtelierPageDateView.swift
@@ -2,12 +2,13 @@
 //  AtelierPageDateView.swift
 //  Cookerz
 //
-//  Created by apprenant98 on 13/03/2025.
+//  Created by Carine ESPEJO on 13/03/2025.
 //
 
 import SwiftUI
 
 struct AtelierPageDateView: View {
+    //call the cooking class to retrieve date infos
     var atelier: Atelier
     
     var body: some View {
@@ -16,6 +17,7 @@ struct AtelierPageDateView: View {
                 .resizable()
                 .frame(width: 20, height: 20)
                 .padding(2)
+            // use the function under to change the date into a string format to show it
             Text(getFormattedDateAndTime())
                 .font(.PoppinsSubhead())
         }
@@ -24,19 +26,24 @@ struct AtelierPageDateView: View {
         .background(Color.red50)
         .cornerRadius(16)
     }
-    
+
+    //function to change the date into a string format to show it
     func getFormattedDateAndTime() -> String {
+        //put the DateFormatter') into a var to use it later
         let dateFormatter = DateFormatter()
 
+        //choose format of the string for date
         dateFormatter.dateFormat = "dd/MM"
+        //retrieve it into a var
         let dateString = dateFormatter.string(from: atelier.date)
-        
+
+        //same with the hour
         let timeFormatter = DateFormatter()
 
         timeFormatter.dateFormat = "HH:mm"
         let timeString = timeFormatter.string(from: atelier.date)
         
-
+        // return the 2 varaibles containing the date and hour into string
         return "\(dateString) \(timeString)"
     }
 }
