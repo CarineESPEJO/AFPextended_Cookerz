efd26982082a9af5affbd71602c0fb9bf3cf8462
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-15 08:45:31 +0200

    Update PageStuffView.swift comments

--- Cookerz/Views/CommonViews/Page Views/PageCommonViews/PageStuffView.swift
+++ Cookerz/Views/CommonViews/Page Views/PageCommonViews/PageStuffView.swift
@@ -2,14 +2,17 @@
 //  PageStuffView.swift
 //  Cookerz
 //
-//  Created by apprenant98 on 13/03/2025.
+//  Created by Carine ESPEJO on 13/03/2025.
 //
 
 import SwiftUI
 
 struct PageStuffView: View {
+    // Binding to change the number of portions
     @Binding var numberPortionChosen : Int
+    //number of portion put by the creator to do conversions
     var numberPortionBase : Int
+    //to show the lists
     var listIngredients : [Ingredient]
     var listUstensils : [Ustensil]
     
@@ -19,9 +22,10 @@ struct PageStuffView: View {
             HStack {
                 Text("Ingrédients")
                 Spacer()
+                //Button to change the number of portions
                 PortionsView(numberPortionChosen: $numberPortionChosen)
             }
-            
+
             PageListIngredientsView(listIngredient: listIngredients , portion: numberPortionBase, numberPortionChosen: $numberPortionChosen )
             
             VStack {
