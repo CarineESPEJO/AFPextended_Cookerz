a6c2f4672078d2ac09810b0c729540fb77f6ea11
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-15 15:55:06 +0200

    Update PickerTimeView.swift comments

--- Cookerz/Views/CommonViews/CreationsViews/CreationCommonViews/CreationPickers/PickerTimeView.swift
+++ Cookerz/Views/CommonViews/CreationsViews/CreationCommonViews/CreationPickers/PickerTimeView.swift
@@ -1,15 +1,17 @@
 //
-//  PickerModzlView.swift
+//  PickeTimeView.swift
 //  Cookerz
 //
-//  Created by apprenant98 on 11/03/2025.
+//  Created by Carine ESPEJO on 11/03/2025.
 //
 
 import SwiftUI
 
 struct PickerTimeView: View {
+    // Need hours and minutes to change them with the picker
     @Binding var TimeHours: Int
     @Binding var TimeMinutes: Int
+    // to dismiss the picker
     @Binding var isPickerPresented : Bool
     var body: some View {
         VStack {
@@ -18,6 +20,7 @@ struct PickerTimeView: View {
                     .foregroundStyle(Color.black)
                 Spacer()
                 Button {
+                    // to dismiss the picker
                     isPickerPresented.toggle()
                 }label:{
                     Text("Valider")
@@ -29,16 +32,21 @@ struct PickerTimeView: View {
             .cornerRadius(10)
             HStack {
                 Spacer()
+                //picker to change the hour of recipe
                 Picker("Heures", selection: $TimeHours) {
+                    //the picker can from 0 to 168 hours
+                    // there is a section for each hour 
                     ForEach(0...168, id: \.self) { hour in
                         Text("\(hour)")
                     }
                 }
+                // wheel style picker (ex: https://media.idownloadblog.com/wp-content/uploads/2022/10/iOS-wheel-style-time-picker.png)
                 .pickerStyle(.wheel)
                 
-                
+                // texte to separate the two wheels
                 Text("h")
-                
+
+                //picker to change the minutes of recipe
                 Picker("Minutes", selection: $TimeMinutes) {
                     ForEach(0...59, id: \.self) { minute in
                         Text("\(minute)")
