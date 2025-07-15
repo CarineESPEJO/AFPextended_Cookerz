60d0c1fa4d5db9742b826899838f7a3218cd37ce
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-15 15:01:28 +0200

    Update RecipeDoublePickerView.swift comments

--- Cookerz/Views/CommonViews/CommonCommonViews/RecipeDoublePickerView.swift
+++ Cookerz/Views/CommonViews/CommonCommonViews/RecipeDoublePickerView.swift
@@ -2,18 +2,23 @@
 //  RecipeDoublePicker.swift
 //  Cookerz
 //
-//  Created by apprenant98 on 14/03/2025.
+//  Created by Carine ESPEJO on 14/03/2025.
 //
 
 import SwiftUI
 
+// Picker to change between the ingredients part and the steps part
 struct RecipeDoublePickerView: View {
     @Binding var selectedPart : RecipeEnum
     var body: some View {
+        //selectedPart is the var to change value
         Picker("Catégorie", selection: $selectedPart) {
+            // text is the name on one of the parts of the picker
+            // tag is the value assigned to one of the parts of the picker
             Text(RecipeEnum.ingredients.name).tag(RecipeEnum.ingredients)
             Text(RecipeEnum.instructions.name).tag(RecipeEnum.instructions)
         }
+        //style of the picker
         .pickerStyle(.segmented)
         .padding(.vertical)
         .colorScheme(.light)
