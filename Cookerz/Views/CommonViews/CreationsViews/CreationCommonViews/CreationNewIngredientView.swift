72811239171ed55bb8b16e27b00b17c35f664d15
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-22 08:52:53 +0200

    Update CreationNewIngredientView.swift commentq

--- Cookerz/Views/CommonViews/CreationsViews/CreationCommonViews/CreationNewIngredientView.swift
+++ Cookerz/Views/CommonViews/CreationsViews/CreationCommonViews/CreationNewIngredientView.swift
@@ -2,24 +2,30 @@
 //  CreationNewIngredientView.swift
 //  Cookerz
 //
-//  Created by apprenant98 on 17/03/2025.
+//  Created by Carine ESPEJO on 17/03/2025.
 //
 
 
 import SwiftUI
 
+//view to create and add a new ingredient to the list of ingredients
 struct CreationNewIngredientView: View {
+    // all this var are here to be put in false when the ingredient picker or the ingredient unit picker are opened
+    // it for when they are also opened to avoid them to stack on each other vertically
     @Binding var isGalleryPresented : Bool
     @Binding var isDateOrTimePickerPresented : Bool
     @Binding var isUstensilTypePickerPresented : Bool
+    //to open the ingredient picker
     @Binding var isIngredientPickerPresented : Bool
-    
+    //to open the ingredient unit picker
+    @Binding var isIngredientUnitPickerPresented : Bool
+
+    //to confirm that the ingredient picker have been use at least one time before letting the user addd the ingredient
     @Binding var ingredientChosen : Bool
     
     @Binding var listIngredients : [Ingredient]
     @Binding var newIngredient : Ingredient
     @Binding var newIngredientQuantity : String
-    @Binding var isIngredientUnitPickerPresented : Bool
     @Binding var newIngredientUnit : IngredientUnit
     
     var body: some View {
@@ -33,6 +39,7 @@ struct CreationNewIngredientView: View {
                         .stroke(Color.grisBordure, lineWidth: 1))
             HStack {
                 Button {
+                    // open the ingredient picker and close the others front views
                     isGalleryPresented = false
                     isDateOrTimePickerPresented = false
                     isUstensilTypePickerPresented = false
@@ -40,8 +47,10 @@ struct CreationNewIngredientView: View {
                     isIngredientPickerPresented = true
                 }label :{
                     HStack {
+                        //when the picker is not already used, the button showw "ingredient"
                         if ingredientChosen == false {
                             Text("Ingredient")
+                        // and when it is, it shows the ingredient chosen to be added to the list
                         } else {
                             HStack {
                                 Image(newIngredient.type.Icon)
@@ -49,6 +58,7 @@ struct CreationNewIngredientView: View {
                                     .frame(width: 20, height: 20)
                                 Text(newIngredient.name)
                                     .lineLimit(1) 
+                                    //if the name is too long for the size of the button, it's truncated with 3 dots at the end
                                     .truncationMode(.tail)
                             }
                         }
@@ -61,14 +71,14 @@ struct CreationNewIngredientView: View {
                     .cornerRadius(100)
                 }
                 Spacer()
+                // text field to write the quantity of the ingredient
                 VStack (alignment: .trailing){
                     TextField ("Qt", text: $newIngredientQuantity)
                         .frame(height: 8)
                         .keyboardType(.numberPad)
+                        // it prevents writing anything other than virgules and points digits to be able to convert the input into a positive float
                         .onChange(of: newIngredientQuantity) { quantity in
                             let filtered = quantity.filter { $0.isNumber || $0 == "." || $0 == "," }
-                            
-
                             if filtered != quantity {
                                 newIngredientQuantity = filtered
                             }
@@ -80,6 +90,7 @@ struct CreationNewIngredientView: View {
                 .cornerRadius(10)
                 
                 Button {
+                    // open the ingredient unit picker and close the others front views
                     isGalleryPresented = false
                     isDateOrTimePickerPresented = false
                     isUstensilTypePickerPresented = false
@@ -87,6 +98,7 @@ struct CreationNewIngredientView: View {
                     isIngredientUnitPickerPresented = true
                 }label :{
                     HStack {
+                        //  for the units we us their initial but the one of piece would be difficult to understand so we use its name
                         Text(newIngredientUnit != .piece ? newIngredientUnit.initial : newIngredientUnit.name )
                     }
                     .foregroundStyle(.black)
@@ -98,9 +110,14 @@ struct CreationNewIngredientView: View {
                 
                 
                 Button{
+                    //we just have to verify if the user used a least one time the ingredient picker and that the quantity is not null
+                    //(as its not obligatory to change the type of the ingredient)
                     if ingredientChosen && !newIngredientQuantity.isEmpty {
+                        // add this ingredient to the ingredient list
                         listIngredients.append(Ingredient(name: newIngredient.name, type: newIngredient.type, quantity: Double(newIngredientQuantity), unit : newIngredientUnit ))
+                       // return the ingredient chosen to false so that the user can't add a new ingredient without using one time the ingredient picker again
                         ingredientChosen = false
+                        // same but with the quantity 
                         newIngredientQuantity = ""
                     }
                     
