1971b84b7c67ec0d40b40c288733395b58c6d44c
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-23 09:09:25 +0200

    Update AtelierPickerDateView.swift comments

--- Cookerz/Views/CommonViews/CreationsViews/AtelierCreationViews/AtelierPickerDateView.swift
+++ Cookerz/Views/CommonViews/CreationsViews/AtelierCreationViews/AtelierPickerDateView.swift
@@ -2,22 +2,26 @@
 //  AtelierPickerDateView.swift
 //  Cookerz
 //
-//  Created by apprenant98 on 17/03/2025.
+//  Created by Carine ESPEJO on 17/03/2025.
 //
 
 
 import SwiftUI
 
 struct AtelierPickerDateView: View {
+    //to confirm that the date picker have been use at least one time before the cooking class can be created
     @Binding var atelierDateChosen: Bool
+    // to dismiss the picker
     @Binding var isPickerPresented: Bool
     @Binding var atelierDate : Date
     var body: some View {
         VStack {
             HStack {
+                //title
                 Text ("Date & horaire")
                     .foregroundStyle(Color.black)
                 Spacer()
+                //button to dismiss picker/validate date
                 Button {
                     atelierDateChosen = true
                     print (atelierDateChosen)
@@ -33,6 +37,7 @@ struct AtelierPickerDateView: View {
             
             HStack {
                 Spacer()
+                //date picker
                 DatePicker("", selection: $atelierDate, displayedComponents: [.date, .hourAndMinute])
                     .datePickerStyle(WheelDatePickerStyle())
                     .pickerStyle(.wheel)
