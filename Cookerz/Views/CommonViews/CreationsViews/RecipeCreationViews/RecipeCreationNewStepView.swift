1a3d21ef09a9dcd87825959cf46ff376fd0b8f4c
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-23 08:58:52 +0200

    Update RecipeCreationNewStepView.swift comments

--- Cookerz/Views/CommonViews/CreationsViews/RecipeCreationViews/RecipeCreationNewStepView.swift
+++ Cookerz/Views/CommonViews/CreationsViews/RecipeCreationViews/RecipeCreationNewStepView.swift
@@ -2,20 +2,25 @@
 //  RecipeCreationNewStepView.swift
 //  Cookerz
 //
-//  Created by apprenant98 on 14/03/2025.
+//  Created by Carine ESPEJO on 14/03/2025.
 //
 
 
 import SwiftUI
 
 struct RecipeCreationNewStepView: View {
+    // var list of steps where will be added the new step
     @Binding var listSteps: [String]
+    //var to contains the new step
     @Binding var newStep: String
     var body: some View {
         HStack {
+            // button to add the new step
             Button {
+                //if the textield is empty it can't add it to the list, else it can
                 if !newStep.isEmpty {
                     listSteps.append(newStep)
+                    // reset textfield
                     newStep = ""
                 }
             } label: {
@@ -23,15 +28,16 @@ struct RecipeCreationNewStepView: View {
             }
             
             ZStack(alignment: .topLeading) {
+                //textfield to write the step
                 TextEditor(text: $newStep)
                     .font(.caption)
                     .background(Color.white)
                     .cornerRadius(8)
-                    .overlay(
+                    .overlay(               // border rounded
                         RoundedRectangle(cornerRadius: 8.0)
                             .stroke(.grisBordure, lineWidth: 2))
-                    .colorScheme(.light)
-                
+                    .colorScheme(.light)    // we can't change the background color as usual, we can just switch that between light and dark
+                //placeholder
                 if newStep.isEmpty {
                     Text("Ajouter une étape")
                         .foregroundColor(.black)
