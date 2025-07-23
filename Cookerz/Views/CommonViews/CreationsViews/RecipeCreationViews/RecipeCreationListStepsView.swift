0f70af16683bc310bf343a6396ed2fd9b39b9fbb
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-23 08:51:26 +0200

    Update RecipeCreationListStepsView.swift comments

--- Cookerz/Views/CommonViews/CreationsViews/RecipeCreationViews/RecipeCreationListStepsView.swift
+++ Cookerz/Views/CommonViews/CreationsViews/RecipeCreationViews/RecipeCreationListStepsView.swift
@@ -2,7 +2,7 @@
 //  RecipeCreationListStepsView.swift
 //  Cookerz
 //
-//  Created by apprenant98 on 14/03/2025.
+//  Created by Carine ESPEJO on 14/03/2025.
 //
 
 
@@ -10,12 +10,14 @@ import SwiftUI
 
 struct RecipeCreationListStepsView: View {
     @Binding var listSteps: [String]
-    
+
+    // view to show the list of steps of the recipe in creation
     var body: some View {
         VStack(alignment: .leading, spacing: 12) {
             ForEach(listSteps, id: \.self) { step in
                 HStack {
                     HStack {
+                        // show the index of the step + 1 because it began at 0
                         Text("\(listSteps.firstIndex(of: step)! + 1)")
                             .font(.PoppinsSubhead())
                     }
@@ -23,14 +25,18 @@ struct RecipeCreationListStepsView: View {
                     .background(Color.red50)
                     .cornerRadius(100)
                     
-                    
+                    // the text of step
                     Text(step)
                         .font(.PoppinsFootnote())
                     
                     Spacer()
-                    
+
+                    // a cross button to delete the step
                     Button {
+                        // it search the index of the step
                         if let index = listSteps.firstIndex(of: step) {
+                            //and use it to delete this step
+                            // (as it doesnt exist anymore, it doesn't show now on the list)
                             listSteps.remove(at: index)
                             
                         }
